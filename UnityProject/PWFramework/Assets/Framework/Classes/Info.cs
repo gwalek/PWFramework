8afa25a5395786f0f24c3480bb30335e43caec08
Grash <grashuriza@gmail.com>
2018-03-02 15:24:45 -0500

    Inital Project Upload
    Inital Project Upload

---
+++ UnityProject/PWFramework/Assets/Framework/Classes/Info.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Information Classes hold some sort of Information in Game. 
+/// Inherits from Actor
+/// This Class implements nothing. Intended as structure in class heirarchy
+/// </summary>
+public class Info : Actor
+{
+
+   
+}
---
+++ UnityProject/PWFramework/Assets/PWGame/Classes/PWInputPoller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PWInputPoller : InputPoller {
+
+    public override InputState GetPlayer1Input()
+    {
+        InputState IS = InputState.GetBlankState();
+        IS.AddAxis("Horizontal", Input.GetAxis("Horizontal"));
+        IS.AddAxis("Vertical", Input.GetAxis("Vertical"));
+        IS.AddButton("Fire1", Input.GetButtonDown("Fire1"));
+        IS.AddButton("Fire2", Input.GetButtonDown("Fire2"));
+        IS.AddButton("Fire3", Input.GetButtonDown("Fire3"));
+        IS.AddButton("Fire4", Input.GetButtonDown("Fire4"));
+        return IS;
+    }
+
+    public override InputState GetPlayer2Input()
+    {
+        InputState IS = InputState.GetBlankState();
+        IS.AddAxis("Horizontal", Input.GetAxis("P2Horizontal"));
+        IS.AddAxis("Vertical", Input.GetAxis("P2Vertical"));
+        IS.AddButton("Fire1", Input.GetButtonDown("P2Fire1"));
+        IS.AddButton("Fire2", Input.GetButtonDown("P2Fire2"));
+        IS.AddButton("Fire3", Input.GetButtonDown("P2Fire3"));
+        IS.AddButton("Fire4", Input.GetButtonDown("P2Fire4"));
+        return IS;
+    }
+}
