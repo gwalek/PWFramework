2b0c7e0100a00bf10dc6b757797ae1789f749d45
Grash <grashuriza@gmail.com>
2018-03-22 17:17:41 -0400

    Implemented Agent Abuse
    Added Rigid Bodies to Agents, I can move them around and they will path back

---
+++ UnityProject/PWFramework/Assets/PWGame/Classes/PWGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PWGame : Game {
+
+    public List<GameObject> SpawnPool;
+
+    public virtual void Start()
+    {
+
+    }
+
+    public virtual GameObject GetCar()
+    {
+        return null; 
+    }
+
+    public virtual void ReturnCar(GameObject c)
+    {
+
+    }
+}
