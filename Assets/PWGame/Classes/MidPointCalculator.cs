47d3e67b2b76d07b13808b86bb6d4e1a6144b218
Grash <grashuriza@gmail.com>
2018-04-25 15:26:42 -0400

    Added CameraMidPointExample

---
+++ Assets/PWGame/Classes/MidPointCalculator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidPointCalculator : MonoBehaviour {
+
+    public GameObject Camera;
+    float startingY; 
+    public GameObject Sphere; 
+    public List<GameObject> Pawns;
+    public List<Vector3> PawnLocations; 
+
+	// Use this for initialization
+	void Start () {
+        startingY = Camera.transform.position.y; 
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetPawnLocations();
+        Sphere.transform.position = FindCenter(PawnLocations);
+
+        Camera.transform.position = Sphere.transform.position;
+        Vector3 location = Camera.transform.position;
+        location.y = startingY;
+        Camera.transform.position = location; 
+    }
+
+    public void GetPawnLocations()
+    {
+        PawnLocations.Clear();
+        foreach (GameObject p in Pawns)
+        {
+            PawnLocations.Add(p.transform.position); 
+        }
+    }
+
+    public Vector3 FindCenter(List<Vector3> PawnLocations)
+    {
+        Vector3 result = Vector3.zero;
+        // Start Small for the Max. Start Large for the Min
+        Vector3 MaxExtent = new Vector3(float.MinValue, float.MinValue, float.MinValue); 
+        Vector3 MinExtent = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+
+        foreach (Vector3 place in PawnLocations)
+        {
+            MaxExtent = Vector3.Max(place, MaxExtent);
+            MinExtent = Vector3.Min(place, MinExtent);
+        }
+        Vector3 Extent = MaxExtent - MinExtent;
+        Extent = Extent * .5f; // Divide  
+        result = MinExtent + Extent; 
+
+        return result;
+    }
+}
