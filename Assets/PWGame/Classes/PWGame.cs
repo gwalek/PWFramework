77fe288a81f3a1f7df258a7becc196de79be29a5
Grash <grashuriza@gmail.com>
2018-05-14 17:09:55 -0400

    Did Someting...

--- Assets/PWGame/Classes/PWGame.cs
+++ Assets/PWGame/Classes/PWGame.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class PWGame : Game {
 
     public List<GameObject> SpawnPool;
-
+    
     public virtual void Start()
     {
 
