7ee0352545ad12eaf9e8646f3128f4726e53fec9
Grash <grashuriza@gmail.com>
2019-02-13 15:17:07 -0500

    Update to 2018.3 and Example Pawn and controller.

---
+++ Assets/PWGame/Classes/AAAPawn.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AAAPawn : PWPawn
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Horizontal(float value)
+    {
+
+    }
+
+    public override void Vertical(float value)
+    {
+
+    }
+
+    public override void Fire1(bool value)
+    {
+        
+        LOG("#" + controller.PlayerNumber + " goes BANG!BANG!BANG!BANG!BANG!BANG!BANG!BANG!BANG!BANG!BANG!BANG!BANG!BANG!BANG!");
+    }
+
+    public override void Fire2(bool value)
+    {
+
+    }
+
+    public override void Fire3(bool value)
+    {
+
+    }
+
+    public override void Fire4(bool value)
+    {
+
+    }
+}
---
+++ Assets/PWGame/Classes/AAAPlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AAAPlayerController : PlayerController
+{
+    public GameObject PawninSceneAtStart; 
+
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        IsHuman = true;
+        LogInputStateInfo = false;
+
+        if (!PossesPawn(PawninSceneAtStart))
+        {
+            LOG_ERROR("Where's the pawn, dude?"); 
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public override void Horizontal(float value)
+    {
+        if (value != 0)
+        {
+            LOG("Del-Horizontal (" + value + ")");
+        }
+    }
+
+    public override void Vertical(float value)
+    {
+        if (value != 0)
+        {
+            LOG("Del-Vertical (" + value + ")");
+        }
+    }
+
+    public override void Fire1(bool value)
+    {
+        if (value)
+        {
+            //doing this in two lines
+            PWPawn p = (PWPawn)PossesedPawn;
+            p.Fire1(value);
+
+            // doing this in one line. 
+            // ((PWPawn)PossesedPawn).Fire1(value); 
+        }
+    }
+
+    public override void Fire2(bool value)
+    {
+        if (value)
+        {
+            LOG("Del-Fire2");
+        }
+    }
+
+    public override void Fire3(bool value)
+    {
+        if (value)
+        {
+            LOG("Del-Fire3");
+        }
+    }
+
+    public override void Fire4(bool value)
+    {
+        if (value)
+        {
+            LOG("Del-Fire4");
+        }
+    }
+}
